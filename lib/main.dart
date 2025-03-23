5642b52c3b7b57968b01fabd98c85c2dd2a71ce2
OIIO <ksywle2648@gmail.com>
2025-03-23 21:13:32 +0900

    메인의 각 버튼 클릭 시 해당 페이지로 이동되게함

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:npg/clock.dart';
+import 'homePage.dart';
 
+// 앱 엔트리
 void main() {
-  runApp(const MyApp());
+  runApp(const NpgProject());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// 루트 위젯
+class NpgProject extends StatelessWidget {
+  const NpgProject({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]); // 화면 세로 고정
+    SystemChrome.setEnabledSystemUIMode(
+      SystemUiMode.immersiveSticky,
+    ); // 화면 전체화면
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 147, 221, 255),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Clock(),
-            SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/IngredientsIco.png'),
-                  const Text("식재료 등록"),
-                ],
-              ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/RefrigeratorIco.png'),
-                  const Text('식재료 관리'),
-                ],
-              ),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {},
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Image.asset('assets/images/RecipeIco.png'),
-                  const Text("레시피 추천"),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+      debugShowCheckedModeBanner: false, // 디버깅 모드 베나 표시
+      home: HomePage(), // homePage.dart의
     );
   }
 }
---
+++ lib/refrigeratorPage.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class RefrigeratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
