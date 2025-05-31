ca72becd51851e0ee72dc58db3719eb0d7fd6451
kguskim <khkim010906@gmail.com>
2025-06-01 00:40:24 +0900

    Update constants.dart

--- asd/lib/config/constants.dart
+++ asd/lib/config/constants.dart
@@ -1,5 +1,5 @@
 // lib/config/constants.dart
 class ApiConfig {
   static const String baseUrl =
-      'https://someone-wc-concepts-sit.trycloudflare.com';
+      'https://because-burns-amanda-antivirus.trycloudflare.com';
 }
