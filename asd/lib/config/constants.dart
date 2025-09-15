2778357ced1635861a742055e855b1b736a2945e
kguskim <khkim010906@gmail.com>
2025-09-15 19:17:36 +0900

    Update constants.dart

--- asd/lib/config/constants.dart
+++ asd/lib/config/constants.dart
@@ -1,5 +1,5 @@
 // lib/config/constants.dart
 class ApiConfig {
   static const String baseUrl =
-      'https://spas-penalties-gradually-awards.trycloudflare.com'; // 맨 뒤에 슬래시 안 빼면 로그인 안됨
+      'https://anyway-lived-lender-var.trycloudflare.com'; // 맨 뒤에 슬래시 안 빼면 로그인 안됨
 }
